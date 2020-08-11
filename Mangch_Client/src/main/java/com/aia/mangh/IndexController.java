79ab650b7da2dacb11a5ad97d0795ec25c7ec845
maiorem <maiorem00@gmail.com>
2020-08-11 12:36:00 +0900

    index update errorpage create

---
+++ Mangch_Client/src/main/java/com/aia/mangh/IndexController.java
@@ -0,0 +1,25 @@
+package com.aia.mangh;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+
+@Controller
+public class IndexController {
+	
+
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String home() {
+		
+		return "index";
+	}
+	
+	@ExceptionHandler(NullPointerException.class)
+	public String handleNullPointerException(NullPointerException e) {
+		e.printStackTrace();
+		return "error/nullPointer";
+	}
+	
+}
