0215f9899e272e422c6776f5c21e8c69b97a3f81
alikwon <63032589+alikwon@users.noreply.github.com>
2020-08-14 09:40:39 +0900

    채팅 UI

--- Mangch_Client/src/main/java/com/aia/mangh/chat/controller/ChatController.java
+++ Mangch_Client/src/main/java/com/aia/mangh/chat/controller/ChatController.java
@@ -1,23 +1,24 @@
 package com.aia.mangh.chat.controller;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.aia.mangh.chat.model.SendMsgInfo;
 
 @Controller
+@RequestMapping("/chat")
 public class ChatController {
-
-	@RequestMapping("/chat")
-	public String chatPage(SendMsgInfo smi,HttpSession session,Model model) {
+	@RequestMapping(method = RequestMethod.GET)
+	public String chatPage(SendMsgInfo smi,HttpServletRequest req,Model model,@RequestParam("nick") String nick) {
 		Member member = new Member();
-		member.setId("test1@naver.com");
-		member.setNick("테스트용");
-		
-		session.setAttribute("loginInfo", member);
+		member.setNick(nick);
+		req.getSession().setAttribute("loginInfo", member);
 		model.addAttribute("msgInfo",smi);
 		return "chatting/chatting";
 	}
